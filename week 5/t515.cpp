449817a54ffc39ba7e7bfadb24bc5abb121ec258
Zhansen Segizbay <supersegizbay@gmail.com>
2022-10-17 10:41:44 +0600

    Changed week 05

--- week 5/t515.cpp
+++ week 5/t515.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int n, k;
 int winner(int n, int k) {
-    if (n == 1) return 1;
+    if (n == 1) return 0;
     return (winner(n-1, k) + k) % n;
 }
 
 int main() {
     cin >> n >> k;
-    cout << winner(n, k);
+    cout << winner(n, k) + 1;
     return 0;
 }
