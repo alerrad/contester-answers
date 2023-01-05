fc354f9161378c48476e4123c1ed52ea3ab3780c
Zhansen Segizbay <supersegizbay@gmail.com>
2023-01-05 22:42:17 +0600

    Better solution

--- week 7/t705.cpp
+++ week 7/t705.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
+
+#define pb push_back
+#define pp pop_back
 
 using namespace std;
 
-int n, k, arr[10000][6];
-void trav(int cur, int step, int row, int col, int targ) {
-    if (row == pow(k, n)) return;
-    arr[row][col] = cur;
-    if ((row + 1) % step == 0) cur++;
-    if (cur > targ) cur = 1;
-    trav(cur, step, row + 1, col, targ);
+int n, k;
+vector<int> q;
+
+// By T. Mukhamedzhan
+void rec(int x) {
+    if(q.size() == n) {
+        for(int i = 0; i < q.size(); ++i) cout << q[i] << ' ';
+        cout << endl;
+        return;
+    }
+    for(int i = x; i <= k; ++i) {
+        q.pb(i);
+        rec(x);
+        q.pp();
+    }
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
     cin >> n >> k;
-    for (int i = 0; i < n; ++i) trav(1, pow(k, n - i - 1), 0, i, k);
-    for (int i = 0; i < pow(k, n); ++i) {
-        for (int j = 0; j < n; ++j) cout << arr[i][j] << ' ';
-        cout << endl;
-    }
+    rec(1);
     return 0;
 }
